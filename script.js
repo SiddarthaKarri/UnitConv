30166bfaea235e51b6403045dac2dc8cfc987d38
Siddartha Karri <147732624+SiddarthaKarri@users.noreply.github.com>
2025-03-05 17:48:58 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
-function temperature(){
-    //To convert celcius to farenheit
-    //(CEL * 9/5) + 32
+function temperature() {
     var c = document.getElementById("celsius").value;
-    var f = (c * 9/5) + 32
-    document.getElementById("fahrenheit").value = f
+    var f = document.getElementById("fahrenheit").value;
+    
+    if (c !== "") {
+        // Convert Celsius to Fahrenheit
+        f = (c * 9/5) + 32;
+        document.getElementById("fahrenheit").value = f;
+    } else if (f !== "") {
+        // Convert Fahrenheit to Celsius
+        c = (f - 32) * 5/9;
+        document.getElementById("celsius").value = c;
+    }
 }
-function weight(){
-    //To convert KGs to Pounds
-    // KG * 2.2
+
+function weight() {
     var kg = document.getElementById("kilo").value;
-    var p = kg * 2.2
-    document.getElementById("pounds").value = p
+    var p = document.getElementById("pounds").value;
+    
+    if (kg !== "") {
+        // Convert Kilograms to Pounds
+        p = kg * 2.2;
+        document.getElementById("pounds").value = p;
+    } else if (p !== "") {
+        // Convert Pounds to Kilograms
+        kg = p / 2.2;
+        document.getElementById("kilo").value = kg;
+    }
 }
-function distance(){
-    //To convert KMs to Miles
-    // KM * 0.62137
+
+function distance() {
     var km = document.getElementById("km").value;
-    var m = km * 0.62137
-    document.getElementById("miles").value = m
+    var m = document.getElementById("miles").value;
+    
+    if (km !== "") {
+        // Convert Kilometers to Miles
+        m = km * 0.62137;
+        document.getElementById("miles").value = m;
+    } else if (m !== "") {
+        // Convert Miles to Kilometers
+        km = m / 0.62137;
+        document.getElementById("km").value = km;
+    }
 }
